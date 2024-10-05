097ced68e3b9ac35421e6d9bb8c03c63a207c5c5
jimztech <ibezimjames20@gmail.com>
2024-10-05 08:55:08 +0200

    new-commits

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-function doSomething() {}
-console.log(doSomething.prototype)
+const MAX_PRIME = 1000000;
 
-const doSomethingFromArrowFunction = () => {};
-console.log(doSomethingFromArrowFunction.prototype);
+function isPrime (n) {
+    for(let i = 2; i <= Math.sqrt(n); i++) {
+        if(n % i === 0){
+            return false;
+        }
+    }
+    return n > 1;
+}
+
+const random = (max) => Math.floor(Math.random() * max);
+
+function generatePrimes(quota) {
+    const primes = [];
+    while (primes.length < quota) {
+        const candidate = random(MAX_PRIME);
+        if (isPrime(candidate)) {
+            primes.push(candidate);
+        }
+    }
+    return primes;
+}
+
+const quota = document.querySelector("#quota");
+const output = document.querySelector("#output");
